5c99df7089f4ddd98d4d826e027496536572b145
Edgar Mantes <edgar.mantes11@gmail.com>
2016-12-16 15:09:52 -0600

    Started to add mongoose for database and to set up Heroku

---
+++ config.js
@@ -0,0 +1,2 @@
+exports.DATABASE_URL = "mongodb://emantes:<Thinkful7!@ds023458.mlab.com:23458/logroom"
+exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,13 +1,75 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Item = require('./models/item');
+
+var config = require('./config');
 
 var app = express();
+
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.get('/home', function(request, response){
-	response.send('GET is working')
+
+// middleware
+var runServer = function(callback) {
+    mongoose.connect(config.DATABASE_URL, function(err) {
+        if (err && callback) {
+            return callback(err);
+        }
+
+        app.listen(config.PORT, function() {
+            console.log('Listening on localhost:' + config.PORT);
+            if (callback) {
+                callback();
+            }
+        });
+    });
+};
+
+if (require.main === module) {
+    runServer(function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+// end of middleware
+
+// routes
+app.get('/home', function(req, res) {
+    Item.find(function(err, items) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        }
+        res.json(items);
+    });
 });
 
+app.post('/items', function(req, res) {
+    Item.create({
+        name: req.body.name
+    }, function(err, item) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        }
+        res.status(201).json(item);
+    });
+});
 
-app.listen(process.env.PORT || 8080);
+app.use('*', function(req, res) {
+    res.status(404).json({
+        message: 'Not Found'
+    });
+});
+// end of routes
+
+
+// app.listen(process.env.PORT || 8080);
 
 exports.app = app;
+exports.runServer = runServer;
