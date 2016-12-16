ba3f9b60250958a87087737e5134c0b9c6cef53b
Edgar Mantes <edgar.mantes11@gmail.com>
2016-12-16 15:27:02 -0600

    Setup database to mLabs

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-exports.DATABASE_URL = "mongodb://emantes:<Thinkful7!@ds023458.mlab.com:23458/logroom"
+exports.DATABASE_URL = 'mongodb://emantes:1234@ds023458.mlab.com:23458/logroom'
+
+
 exports.PORT = process.env.PORT || 8080;
